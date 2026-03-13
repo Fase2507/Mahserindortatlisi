f64795c884d3e11f794eda95c5eaf0ad73d5c72d
Fase2507 <fase2507@gmail.com>
2026-03-13 14:56:19 +0300

    sss kısmı tamam developa pushlanabilir

--- next.config.ts
+++ next.config.ts
@@ -3,13 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
-  // Allow running dev server from non-localhost origins (e.g. VM / LAN IP)
-  // See: Next.js warning about allowedDevOrigins in future major versions.
-  allowedDevOrigins: [
-    "http://localhost:3000",
-    "http://127.0.0.1:3000",
-
-  ],
 };
 
 export default nextConfig;
